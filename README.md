d3ad303f3613575fce4798812622ea7517656bba
BohdanKasian <psaiker12@gmail.com>
2018-07-24 18:51:22 +0300

    Initialize repo with almost fully working application.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Realtime apps without backend code
+
+## Create Firebase application
+[Choose between Cloud Firestore and Realtime Database](https://firebase.google.com/docs/database/rtdb-vs-firestore?authuser=0)
+
+## Add several messages from Firebase console
+![Initial DB structure](http://img.in6k.com/screens/fb-meetup-initial-db-structure.png)
+
+## Get history using HTTP
+`GET {firebase-app-host}/messages`
+
+## Add one more message
+NOTE: we get new messages only after page reload
+
+## Import and configure firebase.js
+
+
+## Subscribe to new messages
+
+
+## Deploy to firebase
---
+++ index.js
@@ -0,0 +1,48 @@
+// subscribe for new messages
+let database = firebase.database();
+let messagesRef = database.ref('/messages');
+messagesRef.on('child_added', (data) => {
+  //console.log(data.val())
+  appendMessage(data.val())
+})
+
+
+let messageList = document.querySelector('#messages');
+function appendMessage(message) {
+    if (isEmpty(message)) return;
+    let messageNode = createMessageNode(message);
+    messageList.appendChild(messageNode);
+    messageNode.scrollIntoView(false);
+    return messageNode;
+}
+
+function isEmpty(text) {
+    return !/\S/.test(text);
+}
+
+let msgTmpl = document.querySelector('#message-template')
+    .content
+    .querySelector('.message');
+function createMessageNode(message) {
+    let messageNode = document.importNode(msgTmpl, true);
+    let time = new Date(message.timestamp).toLocaleTimeString();
+    console.log(message);
+    messageNode.querySelector('.body').textContent = message.body;
+    messageNode.querySelector('.time').textContent = time;
+    return messageNode;
+}
+
+let messageForm = document.querySelector('#message-form');
+messageForm.onsubmit = (event) => {
+    event.preventDefault();
+    let messageInput = messageForm.elements['message'];
+    let message = {
+      body: messageInput.value,
+      timestamp: Date.now()
+    }
+    console.log(message);
+    messagesRef.push(message)
+
+    messageForm.reset();
+    messageInput.focus();
+};
